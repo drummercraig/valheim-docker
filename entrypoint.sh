1b6fcbfa1df3d548f08ad7eb12d0fe44ab05389f
drummercraig <165443900+drummercraig@users.noreply.github.com>
2026-01-01 15:25:40 -0500

    Create directory structure and set permissions
    Added commands to create necessary directory structure and set permissions for Valheim data.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,14 @@ set -a
 . /settings.env
 set +a
 
+# Ensure folder structure exists
+mkdir -p /valheim-data/BepInEx/plugins /valheim-data/BepInEx/patchers /valheim-data/BepInEx/config
+mkdir -p /valheim-data/worlds /valheim-data/backups
+
+# Apply initial permissions
+chown -R ${PUID}:${PGID} /valheim-data
+chmod -R 755 /valheim-data
+
 /install_steamcmd.sh
 /install_valheim.sh
 /install_modloader.sh
