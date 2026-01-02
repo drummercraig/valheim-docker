cffcdfb8294260daee74086b631daf910e9c6f79
drummercraig <drummer.craig@outlook.com>
2026-01-02 15:24:05 -0500

    update

--- backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source ./settings.env
-while true; do
-    sleep $BACKUP_SAVEINTERVAL
-    TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-    tar -czf $WORLD_BACKUP/world_$TIMESTAMP.tar.gz $WORLD_SRC
-    find $WORLD_BACKUP -type f -mtime +$BACKUP_LONG -delete
-done
